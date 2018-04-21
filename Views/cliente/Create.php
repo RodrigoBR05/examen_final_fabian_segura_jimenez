40032340464e0eaf347f75b0141b8be862a6a0dd
Unknown <cholo.5@hotmail.es>
2018-04-21 05:53:21 -0600

    asda

--- Views/cliente/Create.php
+++ Views/cliente/Create.php
@@ -114,4 +114,24 @@ if (isset($_SESSION['admin'],$_SESSION['tipo'])) {
         </div>
       </div>
 </main>
+
+<script>
+    $(document).ready(function () {
+        $("#enlaceajax").click(function (evento) {
+            $.ajax({
+                dataType: "json",
+                url: "https://ubicaciones.paginasweb.cr/provincias.json",
+                data: {},
+                success: function (data) {
+                    var html = "<select>";
+                    for(key in data) {
+                        html += "<option value='"+key+"'>"+data[key]+"</option>";
+                    }
+                    html += "</select";
+                    $('#destino').html(html);
+                }
+            });
+        });
+    })
+</script>
 <?php }else{ header('Location: '.URL.'autenticacion');}?>
